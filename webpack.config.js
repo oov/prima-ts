d67ac1898244a66b857a02b5168840a9addebf63
oov <ilovevipper@gmail.com>
2017-07-22 01:11:12 +0900

    export as umd

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ module.exports = {
     entry: './index.ts',
     output: {
         filename: './index.js',
+	library: 'Prima',
+	libraryTarget: 'umd'
     },
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
